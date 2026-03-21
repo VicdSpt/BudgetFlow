dad39a43330800a48e19d82852ca9d0aeda6cdf8
Victor dS <victordespirlet@gmail.com>
2026-03-21 12:47:48 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>BudgetFlow</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
