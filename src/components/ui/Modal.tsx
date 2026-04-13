c3217443523a95f6132d3d03604fe2e09ed1bf02
Victor dS <victordespirlet@gmail.com>
2026-04-13 16:28:05 +0200

    update goals

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom"
+import { X } from 'lucide-react';
 
 interface ModalProps {
     isOpen: boolean;
@@ -16,7 +17,7 @@ export default function Modal({ isOpen, onClose, children, title }: ModalProps) 
             <div className="bg-white rounded-lg p-6 min-w-80 max-w-lg w-full">
                 <div className="flex justify-between items-center mb-4">
                     {title && <h2 className="text-xl font-bold">{title}</h2>}
-                    <button onClick={onClose} className="text-gray-500 hover:text-black text-2xl">X</button>
+                    <button onClick={onClose} className="text-gray-500 hover:text-black text-2xl cursor-pointer"><X size={24}/></button>
                 </div>
                 <div>{children}</div>
             </div>
--- src/pages/GoalsPage.tsx
+++ src/pages/GoalsPage.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react"
+import GoalForm from "../features/goals/components/GoalForm"
+import GoalList from "../features/goals/components/GoalList"
+import Modal from "../components/ui/Modal"
+import Button from "../components/ui/Button"
+
 export default function GoalsPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
-    <div>GoalsPage</div>
+    <div>
+      <div>
+        <h1>Mes Objectifs</h1>
+        <Button variant="primary" onClick={() => setIsModalOpen(true)}>+ Ajouter un Objectif</Button>
+      </div>
+
+      <GoalList />
+
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} title="Nouvel Objectif">
+        <GoalForm onClose={() => setIsModalOpen(false)}/>
+      </Modal>
+    </div>
   )
 }
