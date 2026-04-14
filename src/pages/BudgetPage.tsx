0238d50901c93ee80d1adeb50944230991580c99
Victor dS <victordespirlet@gmail.com>
2026-04-14 18:29:09 +0200

    done goal + budget

--- src/pages/BudgetPage.tsx
+++ src/pages/BudgetPage.tsx
@@ -1,5 +1,16 @@
+import BudgetForm from "../features/budget/components/BudgetForm"
+import BudgetSummary from "../features/budget/components/BudgetSummary"
+
 export default function BudgetPage() {
   return (
-    <div>BudgetPage</div>
+    <div>
+      <h1>Budget</h1>
+      <div>
+        <div>
+          <BudgetForm/>
+          <BudgetSummary/>
+        </div>
+      </div>
+    </div>
   )
 }
